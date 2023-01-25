df3162dec210710c442ca4a97f09b89a364b6e5d
Amit-limbasiya <amit.limbasiya@marutitech.com>
2023-01-25 18:13:10 +0530

    final code

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
-﻿// we can notify another thread or we can pass the data to another thread so that by that information we can put the inputtaker thread into a deep sleep  
-//and we can notify after taking input for migration thread.
-using System;
-using System.Data;
-using System.Globalization;
-using System.Text;
+﻿using System.Text;
 using MySql.Data.MySqlClient;
 namespace practice_sqlserver
 {
+
     class Program
     {
+        static AutoResetEvent interruptionFlag= new AutoResetEvent(false);
+        static bool signal=false;
         static string connectionDetails = @"server=localhost; port=4306; uid=amitml; pwd=Amit@123; database=mydatabase; charset=utf8; sslMode=none";
         static MySqlConnection conn = new MySqlConnection(connectionDetails);
         static MySqlCommand cmd = conn.CreateCommand();
+
+        static bool working=true;
 
         static Dictionary<string, bool> parameters = new Dictionary<string, bool>()
         { { "stop",false } , { "status" , false } };
@@ -24,7 +24,7 @@ namespace practice_sqlserver
             return a + b;
         }
 
-        static void TakeUserRange(out int begin,out int end)      //taking user input for beginning and end of the input.
+        static void TakeUserRange(out int begin,out int end)      
         {
             bool validbegin = false;
             bool validend = false;
@@ -48,32 +48,36 @@ namespace practice_sqlserver
 
         static void Main(string[] args)
         {
-            //taking user input for beginning and end of the input.
-           
-            int begin, end;
-            TakeUserRange(out begin,out end);
 
-            //Thread inptakerThread = new Thread(InputTaker);
+            int begin, end;
+            TakeUserRange(out begin, out end);
             Thread migrationThread = new Thread(() => Migration_main(begin, end));
             migrationThread.Start();
 
-            //inptakerThread.Start();
-            //inptakerThread.Join();
-            while (true)
+            Thread InputThread= new Thread(ConsoleInput);
+            InputThread.Start();
+        }
+
+        private static void ConsoleInput()
+        {
+            while (working)
             {
-                string order=Console.ReadLine();
-                if (order.Equals("Cancel"))
+                if (Console.KeyAvailable)
                 {
-                    parameters["stop"] = true;
-                    migrationThread.Join();
-                    break;
-                }
-                else if (order.Equals("Status"))
-                {
-                    parameters["status"] = true;
+                    signal = true;
+                    string order = Console.ReadLine();
+                    if (order.Equals("Cancel"))
+                    {
+                        parameters["stop"] = true;
+                    }
+                    else if (order.Equals("Status"))
+                    {
+                        parameters["status"] = true;
+                    }
+                    signal = false;
+                    interruptionFlag.Set();
                 }
             }
-
         }
 
         public static void TruncateTable(string tableName)
@@ -86,134 +90,127 @@ namespace practice_sqlserver
         {
             Random random = new Random();
             int id = 1;
-            //for (i = 0; i < 10000 && !parameters["stop"]; i++)  // if we write it then when it will stop it will do i++ unneccessary
-
+           
             for (int i = 0; i < 10000; i++)
             {
                 sb = new StringBuilder("insert into sourcetable (id,number1,number2) values ");
                 list = new List<string>();
                 for (int j = 1; j <= 100; j++)
                 {
-                    //list.Add(string.Format("({0},{1},{2})", id, j, 100 - j));
                     list.Add(string.Format("({0},{1},{2})", id, random.Next(1, 20), random.Next(1, 20)));
                     id++;
                 }
                 sb.Append(string.Join(',', list));
                 cmd = conn.CreateCommand();
-                //cmd.CommandType = System.Data.CommandType.Text;
                 cmd.CommandText = sb.ToString();
                 cmd.ExecuteNonQuery();
             }
             return true;
         }
 
-        public static int Migration(int begin, int end,List<string> list)
+        public static int WriteInDestinationTable(int begin, int end, List<string> list,int totalcount)
         {
             int k, i = 0;
-            int iterations = (int)Math.Ceiling((end - begin) / 100m);
-            for (k = 0; k < iterations; k++)
+            sb = new StringBuilder("insert into destinationtable (id,foreignID,sum) values");
+
+            MySqlConnection writeConn = new MySqlConnection(connectionDetails);
+            writeConn.Open();
+            MySqlCommand writeCmd = writeConn.CreateCommand();
+
+            for (i = begin; i < end; i++)
             {
-                sb = new StringBuilder("insert into destinationtable (id,foreignID,sum) values");
-                int recentbegin = k * 100;
-
-                int recentend = list.Count < (recentbegin + 100) ? list.Count : recentbegin + 100;
-
-                for (i = recentbegin; i < recentend; i++)
+                if (signal)
                 {
-                    if (parameters["stop"]) break;
-                    sb.Append(list[i] + ',');
-
-                    if (parameters["status"])
-                    {
-                        Console.WriteLine(i + " record are inserted till now...");
-                        Console.WriteLine((list.Count - i) + " record are Remaining to migrate...");
-                        parameters["status"] = false;
-                    }
+                    interruptionFlag.WaitOne();
                 }
+                if (parameters["stop"]) break;
+                sb.Append(list[i] + ',');
 
-                if (i != recentbegin)
+                if (parameters["status"])
                 {
-                    string commandString = sb.ToString();
-                    commandString = commandString.Remove(commandString.Length - 1, 1);
-                    cmd.CommandText = commandString;
-                    cmd.ExecuteNonQuery();
-                    Thread.Sleep(5000);
-                    Console.WriteLine("iteration: " + k);
-                }
-
-                if (parameters["stop"])
-                {
-                    Console.WriteLine("Migration Cancelled...");
-                    break;
+                    Console.WriteLine(i + " record are inserted till now...");
+                    Console.WriteLine((totalcount - i) + " record are Remaining to migrate...");
+                    parameters["status"] = false;
                 }
             }
+
+            if (i != begin)
+            {
+                string commandString = sb.ToString();
+                commandString = commandString.Remove(commandString.Length - 1, 1);
+                writeCmd.CommandText = commandString;
+                writeCmd.ExecuteNonQuery();
+
+            }
+
+            if (parameters["stop"])
+                Console.WriteLine("Migration Cancelled...");
+            else
+                Console.WriteLine("Migration chunk done...");
+            writeConn.Close();
             return i;
         }
-        public static List<string> ReadingRecords(int begin,int end,out bool isRowsAvailable,int id)
+        public static List<string> Migrate(int begin,int end,int id)
         {
             cmd.CommandText = string.Format("select * from sourcetable where id between {0} and {1}", begin, end);
             MySqlDataReader reader = cmd.ExecuteReader();
-
+            int counter = 0;
+            int totalcount = end - begin;
+            Console.WriteLine(totalcount);
             list = new List<string>();
             if (reader.HasRows)
             {
                 int sum = 0;
                 while (reader.Read())
                 {
+                    if (counter % 100==0 && counter!=0)
+                    {
+                        WriteInDestinationTable(counter-100,counter,list,totalcount);
+                    }
                     if (parameters["stop"]) break;
+                    
                     sum = DoSum(reader.GetInt32(1), reader.GetInt32(2));
                     list.Add(string.Format("({0},{1},{2})", id, reader.GetInt32(0), sum));
                     id++;
+                    counter++;
+                }
+                if (counter % 100 != 0 && !parameters["stop"])
+                {
+                    int rbegin=counter-(counter%100);
+                    WriteInDestinationTable(rbegin,counter,list,totalcount);
                 }
                 reader.Close();
-                isRowsAvailable = true;
             }
             else
             {
                 Console.WriteLine("No rows found...");
-                isRowsAvailable= false;
             }
             return list;
         }
         public static void Migration_main(int begin, int end)
         {
             System.Console.WriteLine("connected...");
-            bool isRowsAvailable;
             int id = 1;
-            //Console.WriteLine(conn);
             using (conn)
             {
                 try
                 {
                     conn.Open();
-                    //truncate the table first
-                    //TruncateTable("sourcetable");
+                    TruncateTable("sourcetable");
 
-                    //adding numbers into the database
-                    //if (AddNumbersToSourceTable())
-                    //    Console.WriteLine("Source table filled...");
+                    if (AddNumbersToSourceTable())
+                        Console.WriteLine("Source table filled...");
 
 
-                    //truncate destination table
                     TruncateTable("destinationtable");
                     
-                    //Migration: add sum into the destination table
                     while (true)
                     {
-                        list=ReadingRecords(begin, end, out isRowsAvailable, id);
-                        if (isRowsAvailable)
-                        {
-                            int num_of_records = Migration(begin, end, list);
-                            Console.WriteLine(num_of_records + " records migrated successfully...");
-                        }
-                        else
-                        {
-                            Console.WriteLine("No rows Found...");
-                        }
-                        
-                        Console.WriteLine("Do you want next iteration? first enter no and then [Y/N]: ");
-                        Thread.Yield();
+                        list=Migrate(begin, end, id);
+                        Console.WriteLine(list.Count+" records migrated...");
+                        Console.WriteLine("Do you want next iteration? [Y/N]: ");
                         string inp = Console.ReadLine();
+                        parameters["stop"] = false;
                         while (inp.Length != 1 || (!inp.Equals("Y") && !inp.Equals("N")))
                         {
                             Console.WriteLine("invalid input..please enter no and then Y/N: ");
@@ -226,6 +223,7 @@ namespace practice_sqlserver
                         }
                         else
                         {
+                            working = false;
                             break;
                         }
                         
@@ -244,6 +242,7 @@ namespace practice_sqlserver
                 finally
                 {
                     System.Console.WriteLine("Successful connection and in finally block...");
+                    Console.WriteLine("press any key to exit...");
                     conn.Close();
                 }
             }
